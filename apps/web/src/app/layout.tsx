32a669eaa61b4dd0fb658c13450ec67540f12199
atheeq-rhxn <atheeq.rhxn@gmail.com>
2026-01-05 20:55:25 +0530

    chore: remove vercel speed insights due to exceeded usage

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../index.css";
@@ -90,7 +89,6 @@ export default function RootLayout({
 					<Providers>{children}</Providers>
 				</AppProviders>
 				<Analytics />
-				<SpeedInsights />
 				<script
 					type="application/ld+json"
 					dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdSoftware) }}
